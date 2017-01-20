18d168e7e7f928a6f0d13dba04db70ef32e94c18
DaffyNinja <marcusday117@gmail.com>
2017-01-21 01:37:16 +1100

    More scripts Started
    Main menu
    
    Game manager

---
+++ WavesProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
