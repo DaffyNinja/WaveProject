93f382fb5dec63b39141f5ec8a3ec8d9d9e34966
DaffyNinja <marcusday117@gmail.com>
2017-01-20 21:57:36 +1100

    Added Project
    Put in project as is

---
+++ WavesProject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    [Header("Land")]
+    public float landSpeed;
+    public bool onLand;
+
+    [Header("Jump")]
+    public float jumpForce;
+
+    public float groundCheck;
+    public bool grounded;
+
+
+    [Header("Water")]
+    public float waterSpeed;
+    public bool inWater;
+
+    public float diveForce;
+
+    // Add Dive
+
+
+    Rigidbody2D rig;
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        rig = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheck), 1 << LayerMask.NameToLayer("Ground"));
+
+        if (onLand)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-landSpeed, 0, 0);
+
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(landSpeed, 0, 0);
+            }
+        }
+        else if (inWater)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-waterSpeed, 0, 0);
+
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(waterSpeed, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                rig.AddForce(new Vector2(0, -diveForce));
+            }
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rig.AddForce(new Vector2(0, jumpForce));
+        }
+
+
+
+
+
+
+    }
+}
---
+++ WavesProject/Assets/Scripts/Wave.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+    static int size = 200; // Number of vertices
+    static float velocityDamping = 0.999999f; // Proprotional velocity damping, must be less than or equal to 1.
+    static float timeScale = 50f;
+
+    float[] newHeight = new float[size];
+    float[] velocity = new float[size];
+
+    GameObject[] vertex = new GameObject[size];
+
+    public GameObject waveSprite;
+
+
+    void Start()
+    {
+        // we'll use spheres to represent each vertex for demonstration purposes
+        for (int i = 0; i < size; i++)
+        {
+            vertex[i] = Instantiate(waveSprite);
+            vertex[i].transform.position = new Vector2(i - size / 2, 0);
+        }
+    }
+
+    void Update()
+    {
+        // Water tension is simulated by a simple linear convolution over the height field.
+        for (int i = 1; i < size - 1; i++)
+        {
+            int j = i - 1;
+            int k = i + 1;
+            newHeight[i] = (vertex[i].transform.position.y + vertex[j].transform.position.y + vertex[k].transform.position.y) / 3.0f;
+        }
+
+        // Velocity and height are updated...
+        for (int i = 0; i < size; i++)
+        {
+            // update velocity and height
+            velocity[i] = (velocity[i] + (newHeight[i] - vertex[i].transform.position.y)) * velocityDamping;
+
+            float timeFactor = Time.deltaTime * timeScale;
+            if (timeFactor > 1f) timeFactor = 1f;
+
+            newHeight[i] += velocity[i] * timeFactor;
+
+            // update the vertex position
+            Vector3 newPosition = new Vector3(vertex[i].transform.position.x, newHeight[i], vertex[i].transform.position.z);
+            vertex[i].transform.position = newPosition;
+        }
+    }
+}
