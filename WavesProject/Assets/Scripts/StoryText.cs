7590f9dc98ddafd7700bf05e74ef38721c148d5d
DaffyNinja <marcusday117@gmail.com>
2017-01-22 04:14:52 +1100

    Made story text stay

--- WavesProject/Assets/Scripts/StoryText.cs
+++ WavesProject/Assets/Scripts/StoryText.cs
@@ -14,31 +14,62 @@ public class StoryText : MonoBehaviour
     int index = 0;
 
     public float textTime;
+    public float textStayTime;
+    float stayTimer;
     float timer;
+
+    bool textStayonScreen;
 
     // Use this for initialization
     void Start()
     {
         timer = 0f;
+        stayTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+
         if (timer >= textTime)
         {
             storyTextUI.enabled = true;
 
-            storyTextUI.text = Storytexts[index];
+           // storyTextUI.text = Storytexts[index];
             index++;
+
+            textStayonScreen = true;
+
             timer = 0;
+        }
+
+
+
+        if (textStayonScreen)
+        {
+            stayTimer += Time.deltaTime;
+
+            storyTextUI.text = Storytexts[index];
+
+            if (stayTimer >= textStayTime)
+            {
+
+
+               
+
+                textStayonScreen = false;
+                stayTimer = 0;
+            }
 
         }
         else
         {
             storyTextUI.enabled = false;
+
         }
 
+
     }
+
 }
