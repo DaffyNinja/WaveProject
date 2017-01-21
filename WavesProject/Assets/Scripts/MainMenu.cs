0b3a11cee208a88a6d2a031dc4de6c4dc4632c94
DaffyNinja <marcusday117@gmail.com>
2017-01-21 11:12:08 +1100

    Main Menu Changes
    Main menu Changes

--- WavesProject/Assets/Scripts/MainMenu.cs
+++ WavesProject/Assets/Scripts/MainMenu.cs
@@ -4,13 +4,52 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public GameObject mainMenuPanel;
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+
+    void Awake()
+    {
+        mainMenuPanel.SetActive(true);
+
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void OptionsButton()
+    {
+        optionsPanel.SetActive(true);
+
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+
+    }
+
+    public void CreditsButton()
+    {
+        creditsPanel.SetActive(true);
+
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+
+
+    }
+
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void BackButton()
+    {
+        mainMenuPanel.SetActive(true);
+
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
 }
