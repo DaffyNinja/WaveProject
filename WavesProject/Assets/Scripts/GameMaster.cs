df850af59ce31dcb5a826171883e575b5abd9b04
DaffyNinja <marcusday117@gmail.com>
2017-01-21 18:33:00 +1100

    Fixed Some code

--- WavesProject/Assets/Scripts/GameMaster.cs
+++ WavesProject/Assets/Scripts/GameMaster.cs
@@ -4,19 +4,95 @@ using UnityEngine.SceneManagement;
 
 public class GameMaster : MonoBehaviour {
 
+    [Header("Dificulty")]
+    [Header("Easy")]
+    public float easyDebriSpwnTime;
+    public float easyDebriSpeed;
+
+    public float easyWaveHeight;
+    public float easyWaveFrequency;
+
+    float easyTime;
+    bool isEasy;
+    [Header("Medium")]
+    public float mediumTime;
+    public bool isMedium;
+
+    public float medDebriSpwnTime;
+    public float medDebriSpeed;
+
+    public float medWaveHeight;
+    public float medWaveFrequency;
+    [Header("Hard")]
+    public float hardTime;
+    public bool isHard;
+
+    public float hardDebriSpwnTime;
+    public float hardDebriSpeed;
+
+    public float hardWaveHeight;
+    public float hardWaveFrequency;
+
+    float difficultyTimer;
+
     public bool pause;
 
-	// Use this for initialization
-	void Start ()
+    public DebrisSpawn debrisSpwn;
+    public Wave_VerII waveManager;
+
+    // Use this for initialization
+    void Start()
     {
         pause = false;
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
 
+        difficultyTimer = 0;
+
+        isEasy = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        difficultyTimer += Time.deltaTime;
+
+        if (difficultyTimer < easyTime) //Easy
+        {
+
+            debrisSpwn.spawnTime = easyDebriSpwnTime;
+            debrisSpwn.speed = easyDebriSpeed;
+
+            waveManager.waveHeight = easyWaveHeight;
+            waveManager.frequency = easyWaveFrequency;
+
+        }
+        else if (difficultyTimer >= mediumTime) //Medium
+        {
+
+            isEasy = false;
+            isMedium = true;
+
+
+            debrisSpwn.spawnTime = medDebriSpwnTime;
+            debrisSpwn.speed = medDebriSpeed;
+
+            waveManager.waveHeight = medWaveHeight;
+            waveManager.frequency = medWaveFrequency;
+        }
+        if (difficultyTimer >= hardTime) // Hard
+        {
+
+            isMedium = false;
+            isHard = true;
+
+            debrisSpwn.spawnTime = hardDebriSpwnTime;
+            debrisSpwn.speed = hardDebriSpeed;
+
+            waveManager.waveHeight = hardWaveHeight;
+            waveManager.frequency = hardWaveFrequency;
+        }
+
+        // Increase dificulty
 
         if (pause == true)
         {
@@ -34,6 +110,6 @@ public class GameMaster : MonoBehaviour {
 
             pause = !pause;
         }
-	
-	}
+
+    }
 }
--- WavesProject/Assets/Scripts/Wave_VerII.cs
+++ WavesProject/Assets/Scripts/Wave_VerII.cs
@@ -5,11 +5,12 @@ using System.Collections;
 public class Wave_VerII : MonoBehaviour {
 
     [Header("Wave Values")]
-    public float width;
+
     //public int density;
     public float startDelay;
     public float waveHeight;
     public float frequency;
+    float width = 0.5f;
 
     [Space(10)]
     public float xpos;
@@ -17,10 +18,17 @@ public class Wave_VerII : MonoBehaviour {
     [Space(10)]
     public float xspeed;
     public float yspeed;
+    [Space(10)]
+    public float generateDistance; //0f
+    public float destroyDistance; //0f
+
+    GameObject player;
 
     int density = 1;
     int particles;
     int size = 200; // Number of vertices
+    int generateCount = 0;
+    int destroyCount = 0;
     //static float velocityDamping = 1f; // Proprotional velocity damping, must be less than or equal to 1.
     static float timeScale = 50f;
 
@@ -44,6 +52,8 @@ public class Wave_VerII : MonoBehaviour {
     [Space(10)]
     public GameObject waveSprite;
 
+    public Transform waveParent;
+
 
     void Start()
     {
@@ -55,10 +65,13 @@ public class Wave_VerII : MonoBehaviour {
 
         vertex = new GameObject[size * 4];
 
+        player = GameObject.FindGameObjectWithTag("Player");
+
         // we'll use spheres to represent each vertex for demonstration purposes
         for (int i = 0; i < size; i++)
         {
             vertex[i] = Instantiate(waveSprite);
+            vertex[i].transform.parent = waveParent;
             vertex[i].transform.position = new Vector2(((float)i - (float)particles / 2f) * width / 1f + xpos, 0 + ypos);
         }
         totalLength = vertex[size - 1].transform.position.x - vertex[0].transform.position.x;
@@ -72,25 +85,28 @@ public class Wave_VerII : MonoBehaviour {
         time += Time.deltaTime;
 
         //generate new waves
-        if(vertex[size-1].transform.position.x < 80f)
+        if (player.transform.position.x > 50f + generateDistance + totalLength * generateCount)
         {
+            generateCount++;
             float j = 200;
             for (int i = size; i < size + particles; i++)
             {
                 vertex[i] = Instantiate(waveSprite);
-                vertex[i].transform.position = new Vector2(( (float)j - (float)particles / 2f) * width / 1f + xpos -4.5f, 0 + ypos);
+                vertex[i].transform.parent = waveParent;
+                vertex[i].transform.position = new Vector2(((float)j + (float)(particles * (generateCount - 1)) - (float)particles / 2f) * width / 1f + xpos, 0 + ypos);
                 j += 1f;
             }
             size += particles;
         }
         //delete old waves
-        if (vertex[particles - 1].transform.position.x < -10f)
+        if (player.transform.position.x > totalLength + destroyDistance + totalLength * destroyCount)
         {
+            destroyCount++;
             for (int i = 0; i < particles; i++)
             {
                 Destroy(vertex[i]);
             }
-            for(int i = 0; i < particles; i++)
+            for (int i = 0; i < particles; i++)
             {
                 vertex[i] = vertex[i + particles];
             }
@@ -113,13 +129,13 @@ public class Wave_VerII : MonoBehaviour {
             //David's code
             if (time >= startDelay && i == size - 1 /*&& triggered == false*/)
             {
-                newHeight[i] = waveHeight * (float)Math.Cos((time * frequency / 12) * 180 / pi);
+                newHeight[i] = waveHeight * (float)Math.Cos((time * frequency / 12) * 180 / pi) + ypos;
                 //triggered = true;
             }
             //if (time >= 3.2f && i == size - 1 /*&& triggered == false*/)
             //{
-             //   newHeight[i] = 0f; //* (float)Math.Cos();
-              //  triggered2 = true;
+            //   newHeight[i] = 0f; //* (float)Math.Cos();
+            //  triggered2 = true;
             //}
         }
 
