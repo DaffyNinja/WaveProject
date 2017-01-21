0f148c67280e9588e4c8366dd7ea6a008ac0ab7b
DaffyNinja <marcusday117@gmail.com>
2017-01-21 18:40:11 +1100

    Fixes to Game scene

---
+++ WavesProject/Assets/Scripts/CameraMoveII.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoveII : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(speed, 0, 0);
+	
+	}
+}
