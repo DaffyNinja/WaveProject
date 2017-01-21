c703aa42c2009effd3af18168bf7d4aee84fca86
DaffyNinja <marcusday117@gmail.com>
2017-01-21 22:34:46 +1100

    Added story text script

---
+++ WavesProject/Assets/Scripts/StoryText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoryText : MonoBehaviour
+{
+
+    public Transform canvas;
+
+    public Text[] Storytexts;
+    public Text storyText;
+    Color texta;
+    int index = 0;
+
+    public float textTime;
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= textTime)
+        {
+            storyText.text = Storytexts[index].text;
+            index++;
+            timer = 0;
+        }
+
+    }
+}
